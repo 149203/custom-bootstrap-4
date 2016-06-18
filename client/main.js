b08f57554d39b473d609633a933f9332fcfe74b1
149203 <149203@gmail.com>
2016-06-18 10:17:56 -0700

    Finished front end design. Moving to back end.

---
+++ client/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+   $('#logo-box').bigtext();
+});
+
+$(window).load(function () {
+   centerLogo();
+});
+
+let resizeId;
+$(window).resize(function () {
+   clearTimeout(resizeId);
+   resizeId = setTimeout(doneResizing, 200);
+});
+
+function doneResizing() {
+   // All the functions you want to execute after resize go here
+   centerLogo();
+}
+
+function centerLogo() {
+   let logoHeight = $('#logo-box').outerHeight();
+   let wrapperHeight = $('.js-img-height').outerHeight();
+   $('#logo-box').css({
+      'margin-top': (wrapperHeight - logoHeight) * 0.48
+   });
+}
